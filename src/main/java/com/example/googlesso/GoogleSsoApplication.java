2cae980b68b6b330dc82c1d3d803492d2788edfd
sumitpatil4 <sumit.patil@accolitedigital.com>
2023-04-05 11:58:31 +0530

    SSO Repo created

---
+++ src/main/java/com/example/googlesso/GoogleSsoApplication.java
@@ -0,0 +1,13 @@
+package com.example.googlesso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleSsoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleSsoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/googlesso/GoogleSsoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.googlesso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleSsoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
